16a03ef92692c6a08eb721a127ad27b4c653553b
Alondra Warren <daisy.codeimmersives.com>
2021-03-09 12:02:36 -0800

    correct objects and work through stretch goal

--- main.js
+++ main.js
@@ -8,76 +8,85 @@ const myCodeImmersivesObj = {
 
 // Below, set the value of property `wait` to be "I object".
 
-wait = 'I object',
+myCodeImmersivesObj.wait = 'I object';
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
 
-isAnExcellentObject = 'true',
+myCodeImmersivesObj.isAnExcellentObject = true;
 
 
 // Add the property `term` and set it to 1.
 
-term = 1,
+myCodeImmersivesObj.term = 1;
 
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
 
-term = 2,
+myCodeImmersivesObj.term++;
 
 
 // Now add a new property called `currentScore` and set it to 3.
 
-currentScore = 3,
+myCodeImmersivesObj.currentScore = 3;
 
 // Now add a new property called `cumulativeScore` and set it to 50.
 
-cumulativeScore = 50,
+myCodeImmersivesObj.cumulativeScore = 50;
 
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
 
-cumulativeScore = 53, // 'currentScore' + 'cumulativeScore'
+myCodeImmersivesObj. cumulativeScore = myCodeImmersivesObj.cumulativeScore += myCodeImmersivesObj.currentScore; // 'currentScore' + 'cumulativeScore'
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
 
-iAmExactlyThisCool = ['soooooo', 'cool']
+myCodeImmersivesObj['I am exactly this cool'] = 'soooooo, cool';
 
 
 // Part 2 - your own object.
 // Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
-
+const me = {};
 
 // Add a `firstName` property and set it to a string.
-firstName = 'Daisy',
 
+me.firstName = 'Daisy';
 
 // Add a `lastName` property and set it to a string.
 
-lastName = 'Warren',
+me.lastName = 'Warren';
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
 
-  fullName = ['Daisy', ' ', 'Warren',]
+  me.fullName = me.firstName + ' ', me.lastName;
 
 
 // Add an `age` property and set it to a number.
-age = 25,
+me.age = 2;
 
 
 // Add an address property and set it to an empty array.
-address = [ ],
+me.address = [];
 
 
 // Now push three strings into that array.
-  address = address.push(['2199 NW Everett St', 'Portland', 'OR'])
+  me.address.push('2199 NW Everett St');
+  me.address.push('Portland');
+  me.address.push('OR');
 
 // Part 3 - setting key-value pairs in-line.
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
 const freeWillObject = {
-  monday: 'False',
+  monday: false,
   food: 'chia pudding',
   time: 11,
   hoursOfCodeImmersives: ['7', '8', '9', '10', '11', '12'] 
-}
+};
 
 // As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
+const freeWillObject = {
+  monday: false,
+  food: 'chia pudding',
+  time: 11,
+  hoursOfCodeImmersives: ['7', '8', '9', '10', '11', '12'] 
+  'login token' : 'ajkbkjwebgk' // this is what we added for the stretch goal
+};
